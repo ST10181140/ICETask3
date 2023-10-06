490cc08c7a03d8fbda40b50e3a83d3f75b021e66
AneesaIsaacs <104387850+ST10181140@users.noreply.github.com>
2023-10-06 22:01:32 +0200

    First Commit

---
+++ Empployee/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Empployee
+{
+    internal class Employee
+    {
+        public string Name { get; set; }   
+        public double Salary { get; set; }
+        public Employee(string name, double salary)
+        { 
+            Name = name;
+            Salary = salary;
+        }
+
+        public static Employee operator +(Employee employee, double percentage)
+        {
+            employee.Salary += employee.Salary * (percentage/100);
+            return employee;
+        }
+    }
+}
---
+++ Empployee/Program.cs
@@ -0,0 +1,16 @@
+﻿using Empployee;
+using System;
+
+ class Program
+{
+    public static void Main(String[] args)
+    {
+        Employee employee = new Employee("Jessica Alba", 60000.00);
+
+        Console.WriteLine("Initial salary:" + employee.Salary);
+
+        employee += 10;
+
+        Console.WriteLine("New salary: " + employee.Salary);
+    }
+}
